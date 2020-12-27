1b8f1e264962fd93c8fccca4ad410916d89c475a
Anvay P <anvay13@gmail.com>
2020-12-27 10:56:20 +0530

    built-ins

--- code/p1.built_in.py
+++ code/p1.built_in.py
@@ -4,9 +4,9 @@
 
 
 def main():
-    d1 = {'p1': 3, 'p2': 5, 'p3': 12, 'p4': 7, 'p5': 4}
-    more_than_2 = ({k: v > 2 for k, v in d1.items()})
-    print(all(more_than_2))
+    print(bytes([1, 3, 99]))
+    print(bytes('this is a good start','utf-8'))
+    print(bytes(4))
 
 
 if __name__ == '__main__':
