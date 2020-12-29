cfd3253c16b6707e07b2f75dee43707794798a66
Anvay P <anvay13@gmail.com>
2020-12-29 19:09:25 +0530

    planer-1

--- z_rough_code/p1.built_in.py
+++ z_rough_code/p1.built_in.py
@@ -1,13 +1,23 @@
 """
 
 """
+import collections
+
+Point = collections.namedtuple('Point', ('x', 'y'))
 
 
-def main():
-    print(bytes([1, 3, 99]))
-    print(bytes('this is a good start','utf-8'))
-    print(bytes(4))
+def are_rectangle_points(p1, p2, p3, p4):
+    return is_right_triangle(p1, p2, p3, p4) or is_right_triangle(p1, p3, p2, p4)
 
 
-if __name__ == '__main__':
-    main()
+def is_right_triangle(p1, p2, p3):
+    return pythagoras_test(p1, p2, p3) or pythagoras_test(p1, p3, p2)
+
+
+def pythagoras_test(p1, p2, p3):
+    return distance_of_points(p1, p3) ** 2 == (distance_of_points(p1, p2) ** 2 + distance_of_points(p2, p3) ** 2)
+
+
+def distance_of_points(p1, p2):  # distance formula d=√((x2-x1)²+(y2-y1)²)
+    return ((p1.x - p2.x) ** 2 - (p1.y - p2.y) ** 2) ** (1 / 2)
+
