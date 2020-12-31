8cab1c47a1e4adf503328fa265f26adc6a93dec5
Anvay P <anvay13@gmail.com>
2020-12-31 19:41:40 +0530

    adding-bloom-filter-test

--- simple-data-structures/bloom-filter/bloom_test.py
+++ simple-data-structures/bloom-filter/bloom_test.py
@@ -1,14 +1,16 @@
 from bloom_filter import BloomFilter
 from random import shuffle
+from humanize import naturalsize
 
 n = 2_00_00_000 # number of items to add
-p = 0.001 # false positive probability
-
+p = 0.000001 # false positive probability
 bloomf = BloomFilter(n, p)
 print("Size of bit array:{}".format(bloomf.size))
+print("Size of bit array:{}".format(naturalsize(bloomf.size/8)))
+print("probability of False positive {} in {}".format(n*p, n))
+
 print("False positive Probability:{}".format(bloomf.fp_prob))
 print("Number of hash functions:{}".format(bloomf.hash_count))
-
 # words to be added
 word_present = ['abound', 'abounds', 'abundance', 'abundant', 'accessable',
                 'bloom', 'blossom', 'bolster', 'bonny', 'bonus', 'bonuses',
@@ -18,20 +20,20 @@ word_present = ['abound', 'abounds', 'abundance', 'abundant', 'accessable',
 # word not added
 word_absent = ['bluff', 'cheater', 'hate', 'war', 'humanity',
                'racism', 'hurt', 'nuke', 'gloomy', 'facebook',
-               'geeksforgeeks', 'twitter', 'access']
+               'monster', 'twitter', 'access']
 
 for item in word_present:
     bloomf.add(item)
 
 shuffle(word_present)
 shuffle(word_absent)
-
+print()
 test_words = word_present[:10] + word_absent
 shuffle(test_words)
 for word in test_words:
     if bloomf.check(word):
         if word in word_absent:
-            print("'{}' is a false positive!".format(word))
+            print("-> '{}' is a false positive!".format(word))
         else:
             print("'{}' is probably present!".format(word))
     else:
